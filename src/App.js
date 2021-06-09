42b04861e11983d0c2bfe2b092b742b97c316b19
Jordan <jra650@uw.edu>
2021-06-08 21:16:33 -0700

    Work on getting first plot

--- src/App.js
+++ src/App.js
@@ -1,27 +1,153 @@
-import React from "react";
-
-const viewHeight = 500;
-const viewWidth = 500;
+import { useFetch } from "./hooks/useFetch";
+import { scaleLinear } from "d3-scale";
+import { extent, max, min, bin, range } from "d3-array";
+import React, { useState } from "react";
 
 
 const App = () => {
+    const [data, loading] = useFetch(
+        "https://raw.githubusercontent.com/Jauerbach650/react-parcel-example-1/main/Pac12Bball.csv"
+    );
+
+    // csv("https://raw.githubusercontent.com/Jauerbach650/react-parcel-example-1/main/Pac12Bball.csv")
+    // .then(data => console.log(data));
+
+    const ADJOEextent = extent(data, (d) => {
+        return +d.ADJOE;
+    });
+
+    const ADJDEextent = extent(data, (d) => {
+        return +d.ADJDE;
+    });
+
+    const BARTHAGextent = extent(data, (d) => {
+        return +d.BARTHAG;
+    });
+        
+    const size = 500;
+    const margin = 50;
+    const axisTextAlignmentFactor = 6;
+    const barcodeLength = 100;
+    const tickLength = 8;
+    const numSize = 15;
+    // WHAT IS THIS???
+    const maxValueOfTMAX = max([1, 2, 3]);
+
+    const yScale = scaleLinear()
+        .domain(ADJOEextent)
+        .range([size - margin, size - 50]); //unit: pixels
+
+    // const yScaleDefense = scaleLinear()
+    // .domain(ADJDEextent)
+    // .range([size - margin, margin]); //unit: pixels
+
+    // const yScalePower = scaleLinear()
+    // .domain(BARTHAGextent)
+    // .range([size - margin, margin]); //unit: pixels
+    
+
+    // WHAT IS THIS???
+
+    // _bins = bin().thresholds(15);
+    // tmaxBins = _bins(
+    //     data.map((d) => {
+    //         return + d.SNWD;
+    //     })
+    // );
+
+    const histogramLeftPadding = 10;
+
+    const [selectedStation, setSelectedStation] = useState(
+        "KALISPELL GLACIER AP"
+      );
+
+    const [selectedMax, setSelectedMax] = useState(
+        "110"
+    );
+
+    const [selectedMin, setSelectedMin] = useState(
+        "-40"
+    );
+
+    console.log(yScale);
+    // console.log(yScaleDefense);
+    // console.log(yScalePower);
+
     return (
-        <svg 
-            style={{border: "1px solid lightgrey", width: viewWidth,
-            height: viewHeight}}
-        >
-            <circle cx={20} cy={20} r="5" />
-            <rect x={200} y={200} width={10} height={10} fill="rgb(230, 230, 230)"/>
-            <rect x={212} y={200} width={10} height={10} fill="rgb(230, 230, 230)"/>
-            <rect x={224} y={200} width={10} height={10} fill="rgb(230, 230, 230)"/>
-            <rect x={236} y={200} width={10} height={10} fill="rgb(230, 230, 230)"/>
-            <rect x={248} y={200} width={10} height={10} />
-            <line x1={20} y1={viewHeight - 20} x2={150} y2={100} stroke="black" />
-            <text x="20" y="35">
-                Price history of 100 randomly selected Pokemon cards!!!
-            </text>
-        </svg>
-    ); // outer brace = js / inner brace = object
+        <div>
+            <h1>Assignment 2: Exploratory Data Analysis INFO 474 SP 2021</h1>
+            <h2>Pac 12 2021 NCAA Basketball Data</h2>
+            <p>{loading && "Loading data!"}</p>
+            <div id='plot1'>
+                <h3>Barcode Plot: Pac 12 Teams</h3>
+                <h4></h4>
+
+            <svg width={size} height={size} style={{ border: "1px solid black" }}>
+                <text 
+                    x={barcodeLength} 
+                    textAnchor="end"
+                    y={size - margin + axisTextAlignmentFactor} 
+                    style={{ fontSize: 15, fontFamily: "Gill Sans, sans serif" }}>
+                        0
+                </text>
+                {/* left 37 */}
+                <text 
+                    x={barcodeLength} 
+                    textAnchor="end"
+                    y={margin + axisTextAlignmentFactor / 2} 
+                    style={{ fontSize: 15, fontFamily: "Gill Sans, sans serif" }}>
+                        100
+                </text>
+
+                <line
+                    x1={size / 4 - tickLength} 
+                    y1={margin} 
+                    x2={size / 4 - tickLength * 2} 
+                    y2={margin}
+                    stroke={"black"}
+                    stroke-width= {"3"}
+                />
+
+                <line
+                    x1={size / 4 - tickLength}
+                    y1={size - margin}
+                    x2={size / 4 - tickLength * 2}
+                    y2={size - margin}
+                    stroke={"black"} 
+                    stroke-width= {"3"}
+                />
+                
+                {data.map((measurement, index) => {
+                    const highlight = measurement.TEAM === "Washington"
+                    return (
+                        <line
+                            key={index} 
+                            x1={size / 2}
+                            y1={yScale(measurement.ADJOE)}
+                            x2={size / 2 + 20}
+                            y2={yScale(measurement.ADJOE)}
+                            stroke={highlight ? "red" : "steelblue"}
+                            strokeOpacity={highlight ? 1 : 0.1}
+                        />
+                    );
+                })}
+            </svg>
+
+            </div>
+           
+
+            <div>
+                <h2>Write-up from Assignment 2</h2>
+                <p></p>
+                <h2>Peer Feedback</h2>
+                <li></li>
+                <li></li>
+                <li></li>
+                <h2>Implemented Feedback</h2>
+                <p></p>
+            </div>
+        </div>
+    );
 };
 
 export default App;
